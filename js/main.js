458817e996e2debc49a6b1436e2191f349721c9f
mtd <mtdolaj@gmail.com>
2019-06-24 21:38:53 +0100

    Images workign okay

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function calcDays(){
         out.innerHTML= "You are an infant and you just clocked 1 :)  <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/infant.jpeg\">";
     }
     else if(exactDay <= 2017 && exactDay > 2015){
-        out.innerHTML= "You are a toddler. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> ";
+        out.innerHTML= "You are a toddler. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/toddler.png\">";
     }
     else if(exactDay <= 2015 && exactDay > 2010) {
         out.innerHTML= "You are still a child. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + " days <br> <img src=\"img/childhood.jpeg\">";
@@ -29,28 +29,28 @@ function calcDays(){
         out.innerHTML= "You are in early adolescense, have fun. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days +"days <br> <img src=\"img/funny.jpeg\" >";
     }
     else if(exactDay <= 2005 && exactDay > 2000){
-        out.innerHTML= "You are in later adolescense. Enjoy childhood whilst it lasts. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/earlyado.png\" >";
+        out.innerHTML= "You are in later adolescense. Enjoy childhood whilst it lasts. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/funny.jpeg\">";
     }
     else if(exactDay <= 2000 && exactDay > 1998 ){
-        out.innerHTML= "You are in early adulthood, please dont get it wrong now! <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/adult.jpeg\">";
+        out.innerHTML= "You are in early adulthood, please don't get it wrong now! <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/adult.jpeg\">";
     }
     else if(exactDay <= 1998 && exactDay > 1988){
-        out.innerHTML= "You are an adult. I'm curious are you married :) ? <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> ";
+        out.innerHTML= "You are an adult. I'm curious are you married :) ? <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/adult2.jpg\">";
     }
     if (years == 18 || years == 21){
         out.innerHTML= "Congratulations, you're legal. Yay!! <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days +"days <br> <img src=\"img/legal.jpeg\">";
     }
     else if(exactDay <= 1988 && exactDay > 1974){
-        out.innerHTML = "You're getting closer to real life, remember life begins at 40 <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days +"days <br> ";
+        out.innerHTML = "You're getting closer to real life, remember life begins at 40 <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days +"days <br> <img src=\"img/life40.png\">";
     }
     else if (exactDay <= 1974 && exactDay >1968){
-        out.innerHTML= "You're middle aged. Get ready to retire. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> ";
+        out.innerHTML= "You're middle aged. Get ready to retire. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/tooold.jpg\">";
     }
     else if (exactDay <= 1968 && exactDay > 1897){
-        out.innerHTML= "You are getting old. Please do good. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> ";
+        out.innerHTML= "You are getting old. Please do good. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/old.png\">";
     }
     else if (exactDay <= 1897 && exactDay > 1867){
-        out.innerHTML = "You're a little above old are you sure you're really alive? <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> you have lived for exactly " + days + "days <br> ";
+        out.innerHTML = "You're a little above old, you sure you're really alive? <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> you have lived for exactly " + days + "days <br> <img src=\"img/tooold.webp\">";
     }
     else if(exactDay <= 1867){
         out.innerHTML= "Woah, you're surely immortal. Please get to your grave before its too late. <br> Birth year: " + exactDay + "<br> Age: " + years + "years old. <br> You have lived for exactly " + days + "days <br> <img src=\"img/immortal.jpeg\">";
